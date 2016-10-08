547fb1fab3e6e0c4f2b87e6328bc5316f0a9f947
stevenknguyen7 <steven38016@gmail.com>
2016-10-07 23:08:27 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,39 @@ var validateEmail = function(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 };
+
+    /*========= FireBase ========*/
+
+document.getElementById("contactsubmit").addEventListener("click", getVisitor);
+document.getElementById("contactsubmit").addEventListener("click", saveVisitor);
+
+function getVisitor(){
+    alert('getVisitor');
+    var dbref = firebase.database().ref('visitors/');
+    dbref.on('value',function(snapshot) {
+    alert(snapshot.val());
+    }, function (errorObject) {
+        alert("read failed: "+errorObject.code);
+    })
+}
+
+function saveVisitor(){
+    alert('calling saveVisitor');
+    var user = document.getElementById('user').value;
+    var email = document.getElementById('email').value;
+    var comment = document.getElementById('comment').value;
+    alert(user);
+    alert(email);
+    alert(comment);
+    // save the data
+    var mydatabase = firebase.database();
+    var dbref  = mydatabase.ref('visitors/').push({
+        username: user,
+        email: email,
+        comment: comment}, function(errorObject) {
+        alert("PushError" + errorObject.code); 
+    });
+    alert(dbref);
+}
+
+
